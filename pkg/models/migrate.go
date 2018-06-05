d75967aaad779e6e6a1952e74aacff60d683dd9b
Jeff Marion <vampirical@gmail.com>
2018-06-05 12:53:43 -0700

    Add namespaces and timestamps

--- pkg/models/migrate.go
+++ pkg/models/migrate.go
@@ -9,20 +9,48 @@ import (
 var create = `
 CREATE EXTENSION IF NOT EXISTS postgis;
 
+CREATE FUNCTION auto_set_updated() RETURNS trigger AS $auto_set_updated$
+	BEGIN
+		NEW.updated := NOW();
+		RETURN NEW;
+	END;
+$auto_set_updated$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS collections (
-	name TEXT PRIMARY KEY,
+	id BIGSERIAL PRIMARY KEY,
+	parent_collection_id BIGINT REFERENCES collections(id) ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+	full_name TEXT UNIQUE,
+	name TEXT NOT NULL,
 	title TEXT NOT NULL,
-	description TEXT NOT NULL
+	description TEXT NOT NULL DEFAULT '',
+	created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	deleted TIMESTAMP WITH TIME ZONE
 );
+CREATE INDEX IF NOT EXISTS collections_created_idx ON features(created);
+CREATE INDEX IF NOT EXISTS collections_updated_idx ON features(updated);
+CREATE INDEX IF NOT EXISTS collections_deleted_idx ON features(deleted);
+
+CREATE TRIGGER collections_auto_set_updated BEFORE UPDATE ON collections
+	FOR EACH ROW EXECUTE PROCEDURE auto_set_updated();
 
 CREATE TABLE IF NOT EXISTS features (
 	id UUID PRIMARY KEY,
-	collection_name TEXT REFERENCES collections(name) NOT NULL,
+	collection_id BIGINT REFERENCES collections(id) NOT NULL ON UPDATE CASCADE ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
 	geometry GEOMETRY(GEOMETRY, 4326) NOT NULL,
-	properties JSONB NOT NULL
+	properties JSONB NOT NULL,
+	created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+	deleted TIMESTAMP WITH TIME ZONE
 );
-CREATE INDEX IF NOT EXISTS features_collection_name_idx ON features(collection_name);
+CREATE INDEX IF NOT EXISTS features_collection_id_idx ON features(id);
 CREATE INDEX IF NOT EXISTS features_geometry_idx ON features USING GIST(geometry);
+CREATE INDEX IF NOT EXISTS collections_created_idx ON features(created);
+CREATE INDEX IF NOT EXISTS collections_updated_idx ON features(updated);
+CREATE INDEX IF NOT EXISTS collections_deleted_idx ON features(deleted);
+
+CREATE TRIGGER features_auto_set_updated BEFORE UPDATE ON features
+	FOR EACH ROW EXECUTE PROCEDURE auto_set_updated();
 `
 
 var drop = `
